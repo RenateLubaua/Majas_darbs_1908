bd2b9368c6c40d9e0ec82a1f30fb3274af05e2a9
RenateLubaua <109433061+RenateLubaua@users.noreply.github.com>
2022-08-16 23:22:03 +0100

    Update main.java

--- main.java
+++ main.java
@@ -7,32 +7,32 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         while (true) {
             System.out.println("Izvēlies: Akmens, Šķēres, Papīrīts");
-            String tavaIzvēle = scan.nextLine();
+            String tavaIzvēle = scan.nextLine().toLowerCase();
             String[] elements = {"Akmens", "Šķēres", "Papīrīts"};
             String datoraIzvēle = elements[new Random().nextInt(elements.length)];
             System.out.println("Datora izvēle -" + datoraIzvēle);
             {
                 if (tavaIzvēle.equals(datoraIzvēle)) {
                     System.out.println("Neizšķirts!");
-                } else if (tavaIzvēle.equals("Akmens")) {
+                } else if (tavaIzvēle.equals("akmens")) {
                     System.out.println(datoraIzvēle.equals("Papīrīts") ? "Tu zaudēji!" : "Tu uzvarēji!");
-                } else if (tavaIzvēle.equals("Šķēres")) {
+                } else if (tavaIzvēle.equals("šķēres")) {
                     System.out.println(datoraIzvēle.equals("Akmens") ? "Tu zaudēji!" : "Tu uzvarēji!");
-                } else if (tavaIzvēle.equals("Papīrīts")) {
+                } else if (tavaIzvēle.equals("papīrīts")) {
                     System.out.println(datoraIzvēle.equals("Šķēres") ? "Tu zaudēji!" : "Tu uzvarēji!");
                 } else System.out.println("Drukas kļūda, mēģini vēlreiz");
                 //        Ternary operator variable = (condition) ? expressionTrue : expressionFalse;
 
                 System.out.println("Vēlies Turpināt? Jā/Nē");
-                String atbilde = scan.nextLine();
-                if (atbilde.equals("Nē")) {
+                String atbilde = scan.nextLine().toLowerCase();
+                if (atbilde.equals("nē")) {
                     System.out.println("Paldies par spēli");
                     break;
-                } else if (atbilde.equals("Jā")){
+                } else if (atbilde.equals("jā")){
                     System.out.println("Veiksmi!");
                 } else System.out.println("Drukas kļūda, mēģini vēlreiz");
 
-                }
             }
         }
     }
+}
