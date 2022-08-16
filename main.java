a861a81926c2ac3e213328ba63dd04afb5e2c20c
RenateLubaua <109433061+RenateLubaua@users.noreply.github.com>
2022-08-16 23:27:29 +0100

    Update main.java

--- main.java
+++ main.java
@@ -8,18 +8,18 @@ public class Main {
         while (true) {
             System.out.println("Izvēlies: Akmens, Šķēres, Papīrīts");
             String tavaIzvēle = scan.nextLine().toLowerCase();
-            String[] elements = {"Akmens", "Šķēres", "Papīrīts"};
+            String[] elements = {"akmens", "šķēres", "papīrīts"};
             String datoraIzvēle = elements[new Random().nextInt(elements.length)];
             System.out.println("Datora izvēle -" + datoraIzvēle);
             {
                 if (tavaIzvēle.equals(datoraIzvēle)) {
                     System.out.println("Neizšķirts!");
                 } else if (tavaIzvēle.equals("akmens")) {
-                    System.out.println(datoraIzvēle.equals("Papīrīts") ? "Tu zaudēji!" : "Tu uzvarēji!");
+                    System.out.println(datoraIzvēle.equals("papīrīts") ? "Tu zaudēji!" : "Tu uzvarēji!");
                 } else if (tavaIzvēle.equals("šķēres")) {
-                    System.out.println(datoraIzvēle.equals("Akmens") ? "Tu zaudēji!" : "Tu uzvarēji!");
+                    System.out.println(datoraIzvēle.equals("akmens") ? "Tu zaudēji!" : "Tu uzvarēji!");
                 } else if (tavaIzvēle.equals("papīrīts")) {
-                    System.out.println(datoraIzvēle.equals("Šķēres") ? "Tu zaudēji!" : "Tu uzvarēji!");
+                    System.out.println(datoraIzvēle.equals("šķēres") ? "Tu zaudēji!" : "Tu uzvarēji!");
                 } else System.out.println("Drukas kļūda, mēģini vēlreiz");
                 //        Ternary operator variable = (condition) ? expressionTrue : expressionFalse;
 
